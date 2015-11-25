e65c23a853f16026db8445a6656a5d51106e2175
tanvi <tapa8728@colorado.edu>
2015-11-25 02:42:28 -0700

    Status logic has been resolved

--- wsgi/experiments.py
+++ wsgi/experiments.py
@@ -77,13 +77,15 @@ def start_exp():
     debug = request.args['debug']
     current_app.logger.info("Log 002 - Subject: %s started task %s in %s platform and %s browser" % (unique_id, experiment_name, platform, browser))
 
-    # Check to see how many records exist for this experiment in Session Table with status > 1
-    
+    # Check to see how many records exist for this experiment in Session Table with status >= 1
+    ## Status >=1 means: User has already started a session for this experiment, but we dont know what stage of the experiemnt did he leave it in and 
+    ## hence we need to dig deeper on that.
+    ## Consider -  A user logs has 4 sessions for CS and has left all 4 times in instructions phase ie status=1
     matches = Session.query.filter((Session.gfgid == unique_id) & (Session.exp_name == experiment_name)).all()
     sessions_found = len(matches)
     current_app.logger.info("Log 003 - Sessions found on this experiment: %s", sessions_found)
 
-    ## exps_done=0 means this user has done no experiments before, so allocate status=1 
+    ## exps_done=0 means this user has done no experiments before, so allocate status=1 and create a new session for him 
     if sessions_found == 0:
         #Allocate Status of 1
         exp_status = 1
@@ -94,7 +96,7 @@ def start_exp():
         db.session.commit()
         current_app.logger.info("Log 004 - added it to participant table")
 
-        ## Add gfgid to "store_user table"
+        ## Add gfgid to "store_user table"  
         user_info = Store_user(gfgid=unique_id)
         db.session.add(user_info)
         db.session.commit()
@@ -109,14 +111,43 @@ def start_exp():
     elif sessions_found > 0:
         # They've already done this experiment, we should find out what status they were at and 
         # can't do another one if they're past status 1
+        ## Option: Re-run the query again on the Session table and pull out those records with status == 3 .. completed
+        status_3 = Session.query.filter((Session.gfgid == unique_id) & (Session.exp_name == experiment_name) & (Session.status == 3)).all()
+        if(len(status_3) == 0):
+            ## Pull out those records with status = 6..Quit Early
+            status_6 = Session.query.filter((Session.gfgid == unique_id) & (Session.exp_name == experiment_name) & (Session.status == 6)).all()
+            if(len(status_6) == 0):
+                ## Pull out those records with status = 2
+                status_2 = Session.query.filter((Session.gfgid == unique_id) & (Session.exp_name == experiment_name) & (Session.status == 2)).all()
+                if(len(status_2 == 0)):
+                    ## Pull out those records with status = 1 
+                    status_1 = Session.query.filter((Session.gfgid == unique_id) & (Session.exp_name == experiment_name) & (Session.status == 1)).all()
+                    if(len(status_1 > 0)):
+                        current_app.logger.info("Log 007 - User is in Status 1 and can continue experiment")
+                        return render_template(experiment_name + "/exp.html", uniqueId=unique_id, experimentName=experiment_name, debug=debug)
+                    else:
+                        current_app.logger.info("Log 008 - Something is not right in the code")
+                        raise ExperimentError('status_incorrectly_set') #this state should not be reached at all. 
+                else:
+                    current_app.logger.info("Log 009 - User is in Status 2 and cannot re-do this experiment now")
+                    raise ExperimentError('already_started_exp')
+            else:
+                current_app.logger.info("Log 010 - User is in status 6 and cannot re-do this experiment now")
+                raise ExperimentError('tried_to_quit')
+        else:
+            current_app.logger.info("Log 011 - User is in status 3 and has alreadu finished this experiment")
+            raise ExperimentError('already_did_exp_hit')
+       
+
+        ## Option 2: Either run another filtered query on the query object "matches"
+        """with_status_1 = matches.query.filter
         part = matches[0]
         print "part - %s", part
         ## Check the Status
         if int(part.status) > 1 and debug == False:
             raise ExperimentError('already_started_exp')
-
-    ## building the URL to render    
-    return render_template(experiment_name + "/exp.html", uniqueId=unique_id, experimentName=experiment_name, debug=debug)
+       ## building the URL to render    
+        return render_template(experiment_name + "/exp.html", uniqueId=unique_id, experimentName=experiment_name, debug=debug) """
 
 
 @experiments.route('/inexp', methods=['POST'])
