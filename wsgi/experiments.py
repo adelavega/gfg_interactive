3283324e8b05b1f1acb525648b8db2fb82535849
Tanvi Parikh <tapa8728@colorado.edu>
2015-11-28 20:59:59 -0700

    Sessions are updated independant of the status

--- wsgi/experiments.py
+++ wsgi/experiments.py
@@ -108,8 +108,12 @@ def start_exp():
         db.session.add(session_info)
         db.session.commit()
         current_app.logger.info("Log 006 - added it to session table")
-        return render_template(experiment_name + "/exp.html", uniqueId=unique_id, experimentName=experiment_name, debug=debug)
-
+        sess = Session.query.filter((Session.gfgid == unique_id) & (Session.exp_name == experiment_name) & (Session.begin_session == current_time)).one()
+        print "sess is: %s", sess
+        print "session id is: %s", sess.session_id
+        ## pass the session id also in the URL.
+        return render_template(experiment_name + "/exp.html", uniqueId=unique_id, experimentName=experiment_name, debug=debug, sessionid=sess.session_id)
+      
     elif sessions_found > 0:
         # They've already done this experiment, we should find out what status they were at and can't do another one if they're past status 1
         ## Option: Re-run the query again on the Session table and pull out those records with status == 3 .. completed
@@ -181,7 +185,6 @@ def enterexp():
     experiment_name = request.form['experimentName']
     session_id = request.form['sessionid']
 
-    current_app.logger.info("Log 012 - User has finished the instructions in session id: %s", session_id)
     #Category_switch should be populated now, ie after status code 2
     try:
         user = Participant.query.filter((Participant.gfgid == unique_id) & (Participant.experimentname == experiment_name)).one()
