4f0cfd40b5a45d8a235a9b601093b5e58e5510bd
adelavega <delavega@colorado.edu>
2016-06-23 16:52:18 -0400

    Reorganized again. WSGI connects but hits error

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 import setuptools
 
 
-
-setuptools.setup(name='gfg-interactive',
+setuptools.setup(name='gfginteractive',
                  version='0.2.1',
                  description='GenesforGood interactive survey module',
                  long_description=open('README.md').read().strip(),
                  author='Alejandro de la Vega',
                  author_email='delavega@colorado.edu',
-                 url='https://github.com/delaveg/gfg-interactive/',
-                 packages=['gfg-interactive'],
+                 url='https://github.com/delavega/gfg_interactive/',
+                 packages=['gfginteractive'],
                  install_requires=[],
                  license='MIT License')
