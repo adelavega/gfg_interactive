880b950be35df48b8030c55120380d7cb449775c
adelavega <delavega@colorado.edu>
2016-06-23 18:52:53 -0400

    Works on gfg dev 2

---
+++ gfg_interactive/app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+""" This module provides the backend Flask server. """
+# Setup flask
+from flask import Flask, render_template
+from experiments import experiments
+#from dashboard import dashboard
+import os
+from database import db
+import ConfigParser
+
+# Load configuration
+Config = ConfigParser.ConfigParser()
+Config.read("/var/www/config.ini")
+
+app = Flask(__name__)
+app.config.from_object(Config.get("General", "config"))
+app.register_blueprint(experiments, url_prefix='/exp')
+
+db.init_app(app)
+
+
+@app.route('/')
+def index():
+    ''' Index route '''
+    return render_template('default.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    ''' Serve favicon '''
+    return app.send_static_file('favicon.ico')
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
--- gfginteractive.wsgi
+++ gfginteractive.wsgi
@@ -2,6 +2,6 @@ activate_this = '/var/www/gfginteractive/venv/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
 import sys
-sys.path.insert(0, "/var/www/gfginteractive/")
+sys.path.insert(0, "/var/www/gfginteractive/gfg_interactive")
 
-from gfg_interactive import app as application
+from app import app as application
