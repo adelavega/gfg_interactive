ddffb77f02bfa1dd419b463fee8218ec4854aa33
Jake Parelman <JmParelman@gmail.com>
2016-10-24 15:13:30 -0600

    updating

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -4,12 +4,12 @@
     var dataHandler = DataHandler(sessionid);
 
     function beginTask(){
+        $(document).ready(function(){
         var warning = "<span style='color:red; font-size:60px'> " + (String.fromCharCode(9888)) + " </span> This task requires 10-15 minutes of your<br><tab>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; undivided attention <br><br> If you don't have time right now, please come back when you have can focus. <br><br> Otherwise, click continue to begin!";
         $('#GameBox').hide();
         $('#InstInfo').html(warning);
         $('#leftButton').hide();
-
-    }
+    })}
 
     function instRun(){
         var InstCount = 0;
