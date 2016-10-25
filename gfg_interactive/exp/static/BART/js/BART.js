d5967e32159ba4f3f78a8b24e9abbf445e9f0478
Jake Parelman <JmParelman@gmail.com>
2016-10-24 21:52:22 -0600

    adding instructions

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -13,7 +13,9 @@
         var warning = "<span style='color:red; font-size:60px'> " + (String.fromCharCode(9888)) + " </span> This task requires 10-15 minutes of your undivided attention. <br><br> If you don't have time right now, please come back when you have can focus. <br><br> Otherwise, click continue to begin!";
         $('#GameBox').hide();
         $('#InstInfo').html(warning);
-        
+        $('#rightButton').click(function(){
+           $('#instinfo').html(InstructionLIst[0]);
+        });
     }
 
     function instRun(){
