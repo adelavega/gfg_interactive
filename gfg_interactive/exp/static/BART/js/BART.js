2531e8de1ffcf39396a5aac6ef787e9d834f19fd
Jake Parelman <JmParelman@gmail.com>
2016-10-24 21:17:51 -0600

    adding instructions

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -2,27 +2,26 @@
     var pumps, popped, cashed, popPoint, trial;
     trial = 0;
     var dataHandler = DataHandler(sessionid);
-
-    function beginTask(){
-        $(document).ready(function(){
-        var warning = "<span style='color:red; font-size:60px'> " + (String.fromCharCode(9888)) + " </span> This task requires 10-15 minutes of your undivided attention <br><br> If you don't have time right now, please come back when you have can focus. <br><br> Otherwise, click continue to begin!";
-        $('#GameBox').hide();
-        $('#InstInfo').html(warning);
-        $('#leftButton').hide();
-        $('#rightButton').click(function(){
-            return true;
-        })
-    });
-    }
-
-    function instRun(){
-        var InstCount = 0;
-        var InstructionLIst = [
+    var InstructionLIst = [
             "Throughout this task, you will be presented with 30 balloons, one at a time. <br><br> You will be asked to inflate these balloons. Every time you choose to iinflate the balloon, it will grow slighty and you will receive one token.",
             "You can choose to stop inflating a balloon at any point and collect your tokens by choosing to 'cash in'. <br><br>once you choose to cash in, you will begin again with a new balloon.",
             "It is your choice to determine how much to pump up the balloon, but be aware that at some point the balloon will explode <br><br>The explosion point varies across balloons, ranging from the first pump to enough pumps to make the balloon fill almost the entire containing box.<br><br> if the balloon explodes, you will lose all of your tokens and move on to the next balloon.",
             "At the end of the task you will view a report of your performance in the task.<br><br> To practice with a few balloons, press continue."
         ];
+    function beginTask(){
+        $(document).ready(function(){
+        var warning = "<span style='color:red; font-size:60px'> " + (String.fromCharCode(9888)) + " </span> This task requires 10-15 minutes of your undivided attention. <br><br> If you don't have time right now, please come back when you have can focus. <br><br> Otherwise, click continue to begin!";
+        $('#GameBox').hide();
+        $('#InstInfo').html(warning);
+        $('#leftButton').hide();
+        $('#rightButton').click(function(){
+           $('#instinfo').html(InstructionLIst[0]);
+        });
+    });
+    }
+
+    function instRun(){
+        var InstCount = 0;
         $(document).ready(function(){
             $('#InstInfo').html(InstructionLIst[InstCount]);
         $('#rightButton').click(function(){
