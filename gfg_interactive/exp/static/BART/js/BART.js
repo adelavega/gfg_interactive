2d5e1277d5816d0e1e7936d6a3bf550ee1330629
Jake Parelman <JmParelman@gmail.com>
2016-11-01 15:56:02 -0600

    instructions

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -143,7 +143,8 @@ PracticeBlock.prototype.Trial = function(exiTrial) {
             vertsub = popsize;
             ctx.drawImage(Im, (canvas.width/2) - popsize/2, (canvas.height/1.5) - vertsub, popsize,popsize);
         },20);
-
+        
+        startTime = new Date().getTime();
         this.interval3 = setInterval(function(){
            if (new Date().getTime() - startTime > 120){
                 clearInterval(this.interval3);
