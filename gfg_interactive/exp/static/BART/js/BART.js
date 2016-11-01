5365f14f9abc0c44932627d5859808ec9c2dbf3d
Jake Parelman <JmParelman@gmail.com>
2016-11-01 09:20:13 -0600

    instructions

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -54,6 +54,7 @@ PracticeBlock.prototype.start = function(exitTrial) {
     $('#inst').hide();
     var canvas = $('#taskcanvas');
     var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
     ctx.fillRect(0,0,150,75);
     $('#taskContainer').show();
     hideButtons();
