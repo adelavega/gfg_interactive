49c17d0521f63c730033975799f5088a628e8617
Jake Parelman <JmParelman@gmail.com>
2016-11-01 14:44:31 -0600

    instructions

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -99,9 +99,9 @@ PracticeBlock.prototype.Trial = function(exiTrial) {
     function pumpGrow() {
 
         ctx.clearRect(0,0,500,500);
-        bWidth += 0;
-        bHeight += 0;
-        vertadd += 0.01;
+        bWidth += 1;
+        bHeight += 1;
+        vertadd += 0.05;
         console.log(bWidth,bHeight);
         ctx.drawImage(balloon, (canvas.width/2) - bWidth/2, ((canvas.height/1.5) - bHeight/2) - vertadd , bWidth, bHeight);
 
@@ -118,7 +118,7 @@ PracticeBlock.prototype.Trial = function(exiTrial) {
         var startTime = new Date().getTime();
         this.interval = setInterval(function(){
             console.log(new Date().getTime() - startTime);
-            if(new Date().getTime() - startTime > 30){
+            if(new Date().getTime() - startTime > 300){
             clearInterval(this.interval);
             }
             pumpGrow();
