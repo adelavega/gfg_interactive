6be9de37537474de293675138280c65f6330e14f
Jake Parelman <JmParelman@gmail.com>
2016-11-01 15:46:58 -0600

    instructions

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -131,6 +131,7 @@ PracticeBlock.prototype.Trial = function(exiTrial) {
         var popsize = pumps * 2;
         var opacity = 1;
         var startTime = new Date().getTime();
+
         this.interval = setInterval(function(){
            if (new Date().getTime() - startTime > 120){
                 clearInterval(this.interval);
@@ -141,6 +142,7 @@ PracticeBlock.prototype.Trial = function(exiTrial) {
             vertsub = popsize;
             ctx.drawImage(Im, (canvas.width/2) - popsize/2, (canvas.height/1.5) - vertsub, popsize,popsize);
         },20);
+
 
         this.interval = setInterval(function(){
            if (new Date().getTime() - startTime > 120){
@@ -154,6 +156,19 @@ PracticeBlock.prototype.Trial = function(exiTrial) {
             ctx.drawImage(Im, (canvas.width/2) - popsize/2, (canvas.height/1.5) - vertsub, popsize,popsize);
         },20);
 
+        startTime = new Date().getTime();
+        this.interval = setInterval(function() {
+            if (new Date().getTime() - startTime > 120){
+                clearInterval(this.interval);
+            }
+            ctx.clearRect(0,0,500,500);
+            ctx.font = "30px Arial";
+            ctx.fillStyle = 'red';
+            ctx.textAlign = 'center';
+            opacity += 0.2;
+            ctx.globalAlpha = opacity;
+            ctx.fillText("Popped!",canvas.width/2, canvas.height/2);
+        },20);
 
     }
 
