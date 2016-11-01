b650bcfd16b0a2a2391c24d21675cffcd016f4d6
Jake Parelman <JmParelman@gmail.com>
2016-11-01 07:56:48 -0600

    instructions

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -49,3 +49,14 @@ Instruct.prototype.buttonClick = function(button) {
         this.exitTrial();
     }
 };
+
+
+BARTTask = {
+    InstructionText : [
+            "Throughout this task, you will be presented with 30 balloons, one at a time. <br><br> You will be asked to inflate these balloons. Every time you choose to iinflate the balloon, it will grow slighty and you will receive one token.",
+            "You can choose to stop inflating a balloon at any point and collect your tokens by choosing to 'cash in'. <br><br>once you choose to cash in, you will begin again with a new balloon.",
+            "It is your choice to determine how much to pump up the balloon, but be aware that at some point the balloon will explode <br><br>The explosion point varies across balloons, ranging from the first pump to enough pumps to make the balloon fill almost the entire containing box.<br><br> if the balloon explodes, you will lose all of your tokens and move on to the next balloon.",
+            "At the end of the task you will view a report of your performance in the task.<br><br> To practice with a few balloons, press continue."
+            ],
+    Instruction: Instruct
+};
