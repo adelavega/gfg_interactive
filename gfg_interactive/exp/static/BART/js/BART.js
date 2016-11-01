c5b41e28ec92fb0d15e89c38b106a23d917c2c65
Jake Parelman <JmParelman@gmail.com>
2016-11-01 11:03:04 -0600

    instructions

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -76,8 +76,7 @@ PracticeBlock.prototype.Trial = function(exiTrial) {
         ctx.fillStyle = 'white';
         ctx.textAlign = 'center';
         ctx.fillText("Cash In",canvas.width/2, 475);
-        balloon.
-        ctx.drawImage(balloon, (canvas.width/2) - balloon.width/2, (canvas.height/2) - balloon.height/2, height = 2, width = 2);
+        ctx.drawImage(balloon, (canvas.width/2) - balloon.width/2, (canvas.height/2) - balloon.height/2, 10, 10);
     }
 
 
