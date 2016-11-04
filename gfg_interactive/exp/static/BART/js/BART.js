ceb225cc0ecc6515138fecfec89071aee86d853e
Jake Parelman <JmParelman@gmail.com>
2016-11-04 12:46:29 -0600

    m

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -56,12 +56,6 @@ PracticeBlock.prototype.start = function(exitTrial) {
 };
 
 
-
-
-
-
-};
-
 BARTTask = {
     warning : "<span style='color:red; font-size:60px'> " + (String.fromCharCode(9888)) + " </span> This task requires 10-15 minutes of your undivided attention <br><br> If you don't have time right now, please come back when you have can focus. <br><br> Otherwise, click continue to begin!",
     InstructionText : [
