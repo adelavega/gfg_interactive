6455e8a43a26b7be57487d338827b0d5ff659507
Jake Parelman <JmParelman@gmail.com>
2016-11-07 14:12:22 -0700

    adding visual effects

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -79,7 +79,7 @@ Task.prototype.start = function(exitTrial) {
                 $('#mainContainer').css({backgroundColor: '#FFB7B7'});
                 $('#cashBox').css({backgroundColor:'#CAC7CA'});
                 $('#cashText').text('Next Balloon').css({opacity:'0'});
-
+                $("#resultText").delay(500).animate({opacity:'1'},{duration:750, easing:"linear", queue:false});
                 $("#poppedIm").css({
                     height: bheight,
                     width: bwidth,
@@ -97,7 +97,7 @@ Task.prototype.start = function(exitTrial) {
                 $('#mainContainer').delay(500)
                     .animate({backgroundColor:'#f8f7ff'},{duration:750,easing:"linear", queue:false});
                 $("#cashText").delay(500).animate({opacity:'1'},{duration:750, easing:"linear", queue:false});
-                $("#resultText").delay(500).animate({opacity:'1'},{duration:750, easing:"linear", queue:false});
+
 
             }
         }
