400da3237070299f667cdf9540ee2de57ae93460
Jake Parelman <JmParelman@gmail.com>
2016-11-07 15:14:33 -0700

    adding visual effects

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -108,7 +108,7 @@ function reset(){
     cashed = false;
     popPoint = Math.floor((Math.random() * 63) + 1);
     $("#pumpText").text(String(pumps) + ' tokens');
-    $("#balloonIm").css({height: '50px',width: '35px',top:'250px',opacity:'1'});
+    $("#balloonIm").css({height: '50px',width: '35px',top:'250px',opacity:'1'}).show();
     $('#resultText').css({top: '10px'});
 
 }
