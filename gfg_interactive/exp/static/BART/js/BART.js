37630b53a1ff768b9bef4be50ce6b29ef8603510
Jake Parelman <JmParelman@gmail.com>
2016-11-08 12:34:23 -0700

    finishing main task updates

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -108,7 +108,7 @@ Task.prototype.start = function(exitTrial) {
             $("#cashText").delay(500).animate({opacity:'1'},{duration:750, easing:"linear", queue:false});
             $('#resultText').delay(500).animate({top: '20px' ,opacity:'1'},{duration:750, easing:'linear',queue:false});
         } else {
-            console.log(trial);
+            console.log(max);
             if (trial == max-1){
                 return exitTrial();
             } else {
