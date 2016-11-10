661d79c30f66eebd92e52a5ffc527ebe164877d2
Jake Parelman <JmParelman@gmail.com>
2016-11-10 09:45:39 -0700

    finishing main task updates

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -78,10 +78,9 @@ Task.prototype.trial = function() {
     var pumps = this.pumps;
     var popPoint = this.pop_point;
     var state = this.state;
-    var tri = this.trialNum;
-
-    $('#pumpBox').click(function(){
-        console.log(tri);
+    var self = this;
+    $('#pumpBox').click(function(self){
+        console.log(self.trialNum);
         if (!state) {
             pumps ++;
             $("#balloonIm").animate({height: '+=3.25px', width: '+=3px', top: '-=3px'}, 50);
