0364fa29c631b32fdc12f42d9deb85be70f1bd0f
Jake Parelman <JmParelman@gmail.com>
2016-11-09 17:08:33 -0700

    finishing main task updates

--- gfg_interactive/exp/static/BART/js/BART.js
+++ gfg_interactive/exp/static/BART/js/BART.js
@@ -47,6 +47,7 @@ function Task(practice, max) {
     this.stepText = practice ? 'Finish Instructions' : 'Finish Task';
     this.max = max;
     this.balloon = 0;
+
     this.reset = function () {
         this.balloon++;
         this.pumps = 0;
@@ -159,5 +160,5 @@ BARTTask = {
         "At the end of the task you will view a report of your performance in the task.<br><br> To practice with a few balloons, press continue."
     ],
     Instruction: Instruct,
-    Task: Task.trial
+    Task: Task.reset
 };
