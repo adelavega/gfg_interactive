6e64306b5ba9b31f128df425ad6ed4621fbd155e
Jake Parelman <JmParelman@gmail.com>
2016-11-18 08:34:22 -0700

    finish instructions added to BART task

--- gfg_interactive/exp/static/BART/js/task.js
+++ gfg_interactive/exp/static/BART/js/task.js
@@ -11,8 +11,9 @@ blocks = [new BARTTask.Instruction(BARTTask.InstructionText[0]),
           new BARTTask.Instruction(BARTTask.InstructionText[1]),
           new BARTTask.Instruction(BARTTask.InstructionText[2]),
           new BARTTask.Instruction(BARTTask.InstructionText[3]),
-          new BARTTask.Task(5,true),
+          new BARTTask.Task(10,true),
           new BARTTask.Instruction(BARTTask.InstructionText[4]),
+          new common.FinishInstructions,
           new BARTTask.Task(30,false),
           new BARTTask.Instruction(BARTTask.InstructionText[5])
          ];
